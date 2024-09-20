bee8f5ca8f0a87784e5218f8f32b971e61d35877
Jorge <gogeandres.navarro@hotmail.com>
2024-09-20 12:16:35 -0500

    Endpoint: Database connection

---
+++ api/config/db_connection.php
@@ -0,0 +1,34 @@
+<?php
+     /*
+    Postgres Database connection
+    development: Jorge
+    */
+
+    //entrar en el navegador 127.0.0.1/beta/api/config/db_conncetion.php 
+
+    //Creacion de variables
+    $host = "localhost"; //127.0.0.1
+    $username    = "postgres";
+    $password   = "unicesmag";
+    $dbname     = "beta";
+    $port       = "5432";
+
+    $data_connectionn = "
+       host     = $host
+       port     = $port
+       dbname   = $dbname
+       user     = $username
+       password = $password
+    ";
+
+    $conn = pg_connect($data_connectionn);
+
+    if (!$conn) {
+        die("Connection failed: ". pg_last_error());
+    }else{
+        echo "Connected successfully";
+    }
+
+    pg_close($conn);
+
+?>
