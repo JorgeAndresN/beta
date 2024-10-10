f587f616f15c181a7e4fde52804c74582b3ff7a9
Jorge <gogeandres.navarro@hotmail.com>
2024-10-10 12:20:08 -0500

    finish registrer

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -4,7 +4,20 @@
    //Get data from register form
    $email = $_POST['email'];	
    $pass = $_POST['passwd'];
+   //Encrypt password with md5 hashing algorithm
    $enc_pass = md5($pass);
+   //query to inset data into user table
+   $query = "SELECT * from users where email = '$email'";
+   $result = pg_query($conn, $query);
+   $row = pg_fetch_assoc($result);
+   if($row) {
+      // echo "Registration successful!";
+      echo "<script>alert('Email already exist!');</script>";
+      header('Refresh:0;url=http://127.0.0.1/beta/api/src/register_form.html');
+      exit();
+   }
+   
+   //validate if email already exists
    //query to inset data into user table
    $query = "INSERT INTO users (email, password) 
              VALUES ('$email', '$enc_pass')";
@@ -13,7 +26,9 @@
    
    //Check if the query was successful
    if($result) {
-      echo "Registration successful!";
+     // echo "Registration successful!";
+     echo "<script>alert('Registration successful!');</script>";
+     header('Refresh:0;url=http://127.0.0.1/beta/api/src/login_form.html');
    } else {
       echo "Registration failes: ";
    }
